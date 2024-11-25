440e7560a0506b99302289f978d075d624ddc1b5
Volker Kohaupt <vkohaupt@freenet.de>
2024-11-25 20:50:41 +0100

    Wayland: add tab convert

--- src/wayland/convert/QvkConvert_wl.cpp
+++ src/wayland/convert/QvkConvert_wl.cpp
@@ -198,63 +198,65 @@ void QvkConvert_wl::slot_convert_mkv_to_mp4(bool)
 #include <gst/gst.h>
 #include <gst/pbutils/pbutils.h>
 
-/* Structure to contain all our information, so we can pass it around */
+// Structure to contain all our information, so we can pass it around
 typedef struct _CustomData
 {
   GstDiscoverer *discoverer;
   GMainLoop *loop;
 } CustomData;
 
-/* Print a tag in a human-readable format (name: value) */
-static void print_tag_foreach (const GstTagList *tags, const gchar *tag, gpointer user_data)
+// Print a tag in a human-readable format (name: value)
+static void print_tag_foreach( const GstTagList *tags, const gchar *tag, gpointer user_data )
 {
   GValue val = { 0, };
   gchar *str;
-  gint depth = GPOINTER_TO_INT (user_data);
+  gint depth = GPOINTER_TO_INT( user_data );
 
-  gst_tag_list_copy_value (&val, tags, tag);
+  gst_tag_list_copy_value( &val, tags, tag );
 
-  if (G_VALUE_HOLDS_STRING (&val))
-    str = g_value_dup_string (&val);
-  else
-    str = gst_value_serialize (&val);
+  if ( G_VALUE_HOLDS_STRING( &val ) ) {
+    str = g_value_dup_string( &val );
+  } else {
+    str = gst_value_serialize( &val );
+  }
 
-  g_print ("%*s%s: %s\n", 2 * depth, " ", gst_tag_get_nick (tag), str);
-  g_free (str);
+  g_print( "%*s%s: %s\n", 2 * depth, " ", gst_tag_get_nick (tag), str );
+  g_free( str );
 
-  g_value_unset (&val);
+  g_value_unset( &val );
 }
 
-/* Print information regarding a stream */
+// Print information regarding a stream
 static void print_stream_info (GstDiscovererStreamInfo * info, gint depth)
 {
   gchar *desc = NULL;
   GstCaps *caps;
   const GstTagList *tags;
 
-  caps = gst_discoverer_stream_info_get_caps (info);
+  caps = gst_discoverer_stream_info_get_caps( info );
 
-  if (caps) {
-    if (gst_caps_is_fixed (caps))
-      desc = gst_pb_utils_get_codec_description (caps);
-    else
+  if ( caps ) {
+    if ( gst_caps_is_fixed( caps ) ) {
+      desc = gst_pb_utils_get_codec_description( caps );
+    } else {
       desc = gst_caps_to_string (caps);
-    gst_caps_unref (caps);
+    }
+    gst_caps_unref( caps );
   }
 
-  g_print ("%*s%s: %s\n", 2 * depth, " ",
-      gst_discoverer_stream_info_get_stream_type_nick (info),
-      (desc ? desc : ""));
+  g_print( "%*s%s: %s\n", 2 * depth, " ",
+      gst_discoverer_stream_info_get_stream_type_nick( info ),
+      ( desc ? desc : "" ) );
 
-  if (desc) {
-    g_free (desc);
+  if ( desc ) {
+    g_free( desc );
     desc = NULL;
   }
 
-  tags = gst_discoverer_stream_info_get_tags (info);
-  if (tags) {
-    g_print ("%*sTags:\n", 2 * (depth + 1), " ");
-    gst_tag_list_foreach (tags, print_tag_foreach, GINT_TO_POINTER (depth + 2));
+  tags = gst_discoverer_stream_info_get_tags( info );
+  if ( tags ) {
+    g_print( "%*sTags:\n", 2 * ( depth + 1 ), " " );
+    gst_tag_list_foreach( tags, print_tag_foreach, GINT_TO_POINTER( depth + 2 ) );
   }
 }
 
@@ -286,8 +288,8 @@ static void print_topology (GstDiscovererStreamInfo * info, gint depth)
   }
 }
 
-/* This function is called every time the discoverer has information regarding
- * one of the URIs we provided.*/
+// This function is called every time the discoverer has information regarding
+// one of the URIs we provided.
 static void on_discovered_cb (GstDiscoverer * discoverer, GstDiscovererInfo * info, GError * err, CustomData * data)
 {
   GstDiscovererResult result;
@@ -331,7 +333,7 @@ static void on_discovered_cb (GstDiscoverer * discoverer, GstDiscovererInfo * in
     return;
   }
 
-  /* If we got no error, show the retrieved information */
+  // If we got no error, show the retrieved information
 
   g_print( "\nDuration: %" GST_TIME_FORMAT "\n", GST_TIME_ARGS( gst_discoverer_info_get_duration( info ) ) );
 
@@ -371,6 +373,7 @@ void QvkConvert_wl::slot_test_dicover(bool)
     slot_test_dicover_1( "file://" + ui->lineEditConvert->text() );
 }
 
+
 void QvkConvert_wl::slot_test_dicover_1( QString filePath )
 {
   CustomData data;
@@ -380,12 +383,12 @@ void QvkConvert_wl::slot_test_dicover_1( QString filePath )
   QByteArray byteArray = file.toUtf8();
   gchar *uri = byteArray.data();
 
-  /* Initialize cumstom data structure */
+  // Initialize cumstom data structure
   memset (&data, 0, sizeof (data));
 
   g_print ("Discovering '%s'\n", uri);
 
-  /* Instantiate the Discoverer */
+  // Instantiate the Discoverer
   data.discoverer = gst_discoverer_new (5 * GST_SECOND, &err);
   if (!data.discoverer) {
     g_print ("Error creating discoverer instance: %s\n", err->message);
@@ -393,30 +396,28 @@ void QvkConvert_wl::slot_test_dicover_1( QString filePath )
 //    return -1;
   }
 
-  /* Connect to the interesting signals */
+  // Connect to the interesting signals
   g_signal_connect (data.discoverer, "discovered", G_CALLBACK (on_discovered_cb), &data);
   g_signal_connect (data.discoverer, "finished", G_CALLBACK (on_finished_cb), &data);
 
-  /* Start the discoverer process (nothing to do yet) */
+  // Start the discoverer process (nothing to do yet)
   gst_discoverer_start (data.discoverer);
 
-  /* Add a request to process asynchronously the URI passed through the command line */
+  // Add a request to process asynchronously the URI passed through the command line
   if (!gst_discoverer_discover_uri_async (data.discoverer, uri)) {
     g_print ("Failed to start discovering URI '%s'\n", uri);
     g_object_unref (data.discoverer);
 //    return -1;
   }
 
-  /* Create a GLib Main Loop and set it to run, so we can wait for the signals */
+  // Create a GLib Main Loop and set it to run, so we can wait for the signals
   data.loop = g_main_loop_new (NULL, FALSE);
   g_main_loop_run (data.loop);
 
-  /* Stop the discoverer process */
+  // Stop the discoverer process
   gst_discoverer_stop (data.discoverer);
 
-  /* Free resources */
+  // Free resources
   g_object_unref (data.discoverer);
   g_main_loop_unref (data.loop);
-
-//  return 0;
 }
